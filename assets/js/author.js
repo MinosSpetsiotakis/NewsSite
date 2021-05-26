396a9fd8f4f177f5cad74d464a349bb1aa36688f
MinosSpetsiotakis <84538356+MinosSpetsiotakis@users.noreply.github.com>
2021-05-26 14:52:12 +0300

    Add files via upload

---
+++ assets/js/author.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function () {
+	$(".ql-editor").attr("name", "textcont");
+	
+	var mySpan = $(".ql-editor").children();
+	var myHTML = $('<div/>').append(mySpan.clone()).html();
+	$("#content").html(myHTML);
+	window.setInterval(ganttEach, 500);
+});
+function ganttEach() {
+	$(".ql-editor").attr("name", "textcont");
+
+	var mySpan = $(".ql-editor").children();
+	var myHTML = $('<div/>').append(mySpan.clone()).html();
+	$("#content").html(myHTML);
+}
